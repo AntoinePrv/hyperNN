0b272c39af091c615cd17c89c4e3a8164bb9955d
Antoine Prouvost <antoine.prouvost.2013_dev@polytechnique.org>
2017-04-07 11:59:05 -0400

    Refacto to train_Mnist parameters

--- problem/train_MINST.py
+++ problem/train_MINST.py
@@ -5,29 +5,33 @@ from keras import regularizers
 
 
 def train_model_s(s, data):
-    activation = ['sigmoid', 'tanh', 'relu', 'softmax'][s[8]]
-    n_couches = s[0]
-    noeuds = s[1]
-    learning_rate = s[2]
-    reg_l1 = s[3]
-    reg_l2 = s[4]
-    moment = s[5]
-    decay = s[6]
-    nesterov = s[7]
-    return train_model(activation,
-                       n_couches,
-                       noeuds,
-                       learning_rate,
-                       reg_l1,
-                       reg_l2,
-                       moment,
-                       decay,
-                       nesterov,
-                       data)
+    return train_model(
+        data,
+        activation=['sigmoid', 'tanh', 'relu', 'softmax'][s[8]],
+        n_couches=s[0],
+        noeuds=s[1],
+        learning_rate=s[2],
+        reg_l1=s[3],
+        reg_l2=s[4],
+        moment=s[5],
+        decay=s[6],
+        nesterov=s[7],
+    )
 
 
-def train_model(activation, n_couches, noeuds, learning_rate, reg_l1, reg_l2,
-                moment, decay, nesterov, data):
+def train_model(data, n_epoch=100, batch_size=200, **kwargs):
+    hparams = {
+        "activation":    "relu",
+        "n_couches":    1,
+        "noeuds":        [50],
+        "learning_rate": .01,
+        "reg_l1":        .001,
+        "reg_l2":        .001,
+        "moment":        .01,
+        "decay":         1e-6,
+        "nesterov":      True
+    }
+    hparams.update(kwargs)
 
     x_train = data["X_train"]
     y_train = data["Y_train"]
@@ -37,34 +41,39 @@ def train_model(activation, n_couches, noeuds, learning_rate, reg_l1, reg_l2,
     network = Input(shape=(dim,))
     input = network
 
-    for i in range(n_couches):
+    for i in range(hparams["n_couches"]):
         network = Dense(
-            noeuds[i], activation=activation,
-            W_regularizer=regularizers.l1_l2(reg_l1, reg_l2)
+            hparams["noeuds"][i],
+            activation=hparams["activation"],
+            W_regularizer=regularizers.l1_l2(
+                hparams["reg_l1"],
+                hparams["reg_l2"]
+            )
         )(network)
 
     network = Dense(
         10, activation="softmax",
-        W_regularizer=regularizers.l1_l2(reg_l1, reg_l2)
+        W_regularizer=regularizers.l1_l2(
+            hparams["reg_l1"],
+            hparams["reg_l2"])
     )(network)
 
     model = Model(input=input, output=network)
 
-    opt = keras.optimizers.SGD(lr=learning_rate,
-                               momentum=moment,
-                               decay=decay,
-                               nesterov=nesterov)
+    opt = keras.optimizers.SGD(lr=hparams["learning_rate"],
+                               momentum=hparams["moment"],
+                               decay=hparams["decay"],
+                               nesterov=hparams["nesterov"])
 
     model.compile(optimizer=opt, loss="categorical_crossentropy",
                   metrics=['accuracy'])
 
     model.fit(x_train, y_train,
-              nb_epoch=20,
-              batch_size=2000,
+              nb_epoch=n_epoch,
+              batch_size=batch_size,
               verbose=0,
               shuffle=True,
               validation_data=(x_valid, y_valid))
 
     loss = model.evaluate(x_valid, y_valid, verbose=0)
-    print('MINST_loss', loss)
     return (loss[1], model)
