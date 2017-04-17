697c27d59cda1eae8ceff267bd16e1c94ae86d50
Antoine Prouvost <antoine.prouvost.2013_dev@polytechnique.org>
2017-04-17 11:39:09 -0400

    Added more on hyperMads to the report

--- report/plots.py
+++ report/plots.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from sklearn.manifold import MDS
 
 
 def accuracy(acc):
@@ -12,4 +13,15 @@ def accuracy(acc):
     plt.xlim(0, len(acc))
 
     plt.legend(loc=4)
+    plt.show()
+
+
+def mds_accuracy(X, acc):
+    X = MDS(n_components=2).fit_transform(X)
+    plt.figure(figsize=(16, 4), dpi=100)
+    cb = plt.scatter(X[:, 0], X[:, 1], c=acc,
+                     cmap=plt.cm.get_cmap('jet'),
+                     vmin=0.1, vmax=1, s=45)
+    plt.colorbar(cb)
+    plt.title("Accuracy in two components MDS view")
     plt.show()
